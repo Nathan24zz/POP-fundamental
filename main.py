a0b4c371604122e4d56caa02447b49bc085473d3
Nathan24zz <nathanael.hutama24@gmail.com>
2026-05-12 10:45:38 +0700

    feat: add Postgre database connection and initialize it

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 from fastapi import FastAPI
 from model import Product
+from database import session, engine
+import database_model
 
+# ----- DATABASE -----
+# make all tables that are mention in database model
+database_model.Base.metadata.create_all(bind=engine)
+
+# usually we are not doing this in real application
+# make data to initialize database
+products = [
+    Product(id=1, name="Phone", description="A smartphone", price=699.9, quantity=50),
+    Product(id=2, name="Laptop", description="A powerful laptop", price=999.9, quantity=30),
+]
+
+# populate db for the first time
+def init_db():
+    db = session()
+
+    # To get how many rows in the database
+    # SELECT COUNT(*) FROM product
+    count = db.query(database_model.Product).count
+
+    if count == 0:
+        for product in products:
+            # first, convert pydantic model --> SQLAlchemy model
+            # model_dump() is method for converting a Pydantic model instance into a standard Python dictionary
+            # the double asterisk (**) is the dictionary unpacking operator
+            db.add(database_model.Product(**product.model_dump()))
+
+    # to save permanently
+    db.commit()
+
+init_db()
+# ----- DATABASE -----
 
 app = FastAPI()
 
@@ -9,11 +42,6 @@ app = FastAPI()
 @app.get("/")
 def greet():
     return "Welcome to World"
-
-products = [
-    Product(id=1, name="Phone", description="A smartphone", price=699.9, quantity=50),
-    Product(id=2, name="Laptop", description="A powerful laptop", price=999.9, quantity=30),
-]
 
 # show all products
 @app.get("/products")
@@ -55,3 +83,4 @@ def delete_product(id: int):
             return "Product Deleted"
 
     return "Products not found"
+# ----- API -----
